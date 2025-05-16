46415088cab3c8dcd6ed64fd6d0773dd026bdc9b
mmamorim <marc.m.amorim@gmail.com>
2025-05-15 21:39:49 -0300

    v1

--- queriesAula11.sql
+++ queriesAula11.sql
@@ -9,3 +9,25 @@ WHERE
     Filme.codPais=Pais.codigo AND
     Filme.codDiretor=Ator.codigo
 
+SELECT 
+    Filme.codigo,
+    Filme.tituloOriginal AS titulo,
+    Genero.nome,
+    ListaGenero.descricao
+FROM
+    Filme, Genero, ListaGenero
+WHERE
+    Filme.codigo=3 AND
+    Filme.codigo=ListaGenero.codFilme AND
+    Genero.codigo=ListaGenero.codGenero
+    
+SELECT 
+    Filme.codigo,
+    Filme.tituloOriginal AS titulo,
+    Ator.nome AS nomeAtor
+FROM
+    Filme, Ator, Elenco
+WHERE
+    Filme.tituloOriginal="Pretty Woman" AND
+    Filme.codigo=Elenco.codFilme AND
+    Ator.codigo=Elenco.codAtor
